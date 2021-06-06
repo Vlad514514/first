4ac7986b673bb6a461619c9cd11a00bef0f57575
Vlad <2novikov514@gmail.com>
2021-06-07 02:55:48 +0300

    First app

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
-console.log(1);
+const number0Films = +prompt('Сколько фильмов ві уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: number0Films,
+    movies: {},
+    actors: {},
+    generes: [],
+    privat: false
+};
+
+const a = prompt('Один из просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
+
+personalMovieDB.movies [a] = b;
+personalMovieDB.movies [c] = d;
+
+console.log(personalMovieDB);
